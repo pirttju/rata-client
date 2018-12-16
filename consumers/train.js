513380f80a31eca96f5686de6d7486ff60e2a7d9
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2018-12-16 22:36:36 +0200

    Update train.js

--- consumers/train.js
+++ consumers/train.js
@@ -1,9 +1,10 @@
 /*
 Process train messages.
 */
-var p = require("../pgconn");
+const p = require("../pgconn");
 const u = require ("../utils");
 const moment = require("moment");
+const async = require("async");
 
 // Reusable set of columns
 const cs_trains = new p.pgp.helpers.ColumnSet(["train_number", "departure_date", "operator_code", "train_type",
@@ -15,7 +16,7 @@ const cs_timetables = new p.pgp.helpers.ColumnSet(["train_number", "departure_da
     "unknown_delay", "actual_time", "diff_in_minutes", "actual_track", "cause_code", "train_ready"], 
     {schema: "rail", table: "timetables"});
 
-    exports.processMessage = function(body, callback) {
+exports.processMessage = function(body, callback) {
     let data;
 
     try {
@@ -29,7 +30,7 @@ const cs_timetables = new p.pgp.helpers.ColumnSet(["train_number", "departure_da
             next();
         });
     }, function(err) {
-        return callback();
+        callback();
     });
 }
 
