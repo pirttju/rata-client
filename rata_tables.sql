daf87580d8eed11790fc0cdd780877180acefe17
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2018-12-28 13:37:08 +0200

    Update rata_tables.sql

--- rata_tables.sql
+++ rata_tables.sql
@@ -4,49 +4,78 @@ CREATE TYPE rata.train_type AS ENUM ('AE','H','HDM','HL','HLV','HSM','HV','IC','
 CREATE TYPE rata.timetable_type AS ENUM ('REGULAR', 'ADHOC');
 
 CREATE TABLE rata.trains (
-    train_number        int,
-    departure_date      date,
+    train_number        int NOT NULL,
+    departure_date      date NOT NULL,
     operator_code       text,
-    train_type          rata.train_type,
+    train_type          rata.train_type NOT NULL,
     commuter_line_id    text,
-    running_currently   boolean,
-    cancelled           boolean,
-    version             bigint,
-    timetable_type      rata.timetable_type,
-    acceptance_date     timestamptz,
-    deleted             boolean,
-    last_modified       timestamptz,
+    running_currently   boolean NOT NULL DEFAULT FALSE,
+    cancelled           boolean NOT NULL DEFAULT FALSE,
+    version             bigint NOT NULL,
+    timetable_type      rata.timetable_type NOT NULL DEFAULT 'REGULAR'::rata.timetable_type,
+    acceptance_date     timestamptz NOT NULL,
+    begin_station       text NOT NULL,
+    begin_time          timestamptz NOT NULL,
+    end_station         text NOT NULL,
+    end_time            timestamptz NOT NULL,
+    last_modified       timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (train_number, departure_date)
 );
 
-CREATE TYPE rata.row_type AS ENUM ('ARRIVAL', 'DEPARTURE');
-CREATE TYPE rata.estimate_source_type AS ENUM ('COMBOCALC', 'LIIKE_AUTOMATIC', 'LIIKE_USER', 'MIKU_USER');
-CREATE TYPE rata.train_ready_source_type AS ENUM ('KUPLA', 'LIIKE', 'PHONE');
+CREATE INDEX ON rata.trains (operator_code);
+CREATE INDEX ON rata.trains (train_type);
 
-CREATE TABLE rata.timetables (
-    train_number        int,
-    departure_date      date,
-    row_key             smallint,
-    train_stopping      boolean,
-    station_code        text,
-    row_type            rata.row_type,
-    commercial_stop     boolean,
+CREATE TYPE rata.train_ready_type AS ENUM ('KUPLA', 'LIIKE', 'PHONE', 'UNKNOWN');
+
+CREATE TABLE rata.timetablerows (
+    train_number        int NOT NULL,
+    departure_date      date NOT NULL,
+    row_index           smallint NOT NULL,
+    train_stopping      boolean NOT NULL DEFAULT FALSE,
+    station             text NOT NULL,
+    commercial_stop     boolean NOT NULL DEFAULT FALSE,
     commercial_track    text,
-    cancelled           boolean,
-    scheduled_time      timestamptz,
-    estimate_source     rata.estimate_source_type,
-    unknown_delay       boolean,
-    actual_time         timestamptz,
-    diff_in_minutes     smallint,
-    actual_track        text,
-    cause_code          text,
+    arr_cancelled       boolean,
+    arr_scheduled       timestamptz,
+    arr_estimate        timestamptz,
+    arr_unknown_delay   boolean,
+    arr_actual          timestamptz,
+    arr_minutes         smallint,
+    arr_cause_code      text,
+    dep_cancelled       boolean,
+    dep_scheduled       timestamptz,
+    dep_estimate        timestamptz,
+    dep_unknown_delay   boolean,
+    dep_actual          timestamptz,
+    dep_minutes         smallint,
+    dep_cause_code      text,
     train_ready         timestamptz,
-    PRIMARY KEY (train_number, departure_date, row_key)
+    train_ready_src     rata.train_ready_type,
+    train_passed        boolean,
+    actual_platform     text,
+    PRIMARY KEY (train_number, departure_date, row_index)
 );
 
-CREATE INDEX ON rata.timetables(station_code);
-CREATE INDEX ON rata.timetables (DATE(scheduled_time AT TIME ZONE 'Europe/Helsinki'));
-CREATE INDEX ON rata.timetables (DATE(actual_time AT TIME ZONE 'Europe/Helsinki'));
+CREATE INDEX ON rata.timetablerows (station);
+CREATE INDEX ON rata.timetablerows (DATE(arr_scheduled AT TIME ZONE 'Europe/Helsinki')) WHERE arr_scheduled IS NOT NULL;
+CREATE INDEX ON rata.timetablerows (DATE(dep_scheduled AT TIME ZONE 'Europe/Helsinki')) WHERE arr_scheduled IS NOT NULL;
+
+CREATE TABLE rata.compositions (
+    train_number        int NOT NULL,
+    departure_date      date NOT NULL,
+    version             bigint NOT NULL,
+    journey_index       smallint NOT NULL,
+    begin_station       text NOT NULL,
+    begin_time          timestamptz NOT NULL,
+    end_station         text NOT NULL,
+    end_time            timestamptz NOT NULL,
+    locomotives         jsonb,
+    wagons              jsonb,
+    total_length        smallint,
+    maximum_speed       smallint,
+    PRIMARY KEY (train_number, departure_date, journey_index)
+);
 
 SELECT create_hypertable('rata.trains', 'departure_date');
-SELECT create_hypertable('rata.timetables', 'departure_date');
+SELECT create_hypertable('rata.timetablerows', 'departure_date');
+SELECT create_hypertable('rata.compositions', 'departure_date');
