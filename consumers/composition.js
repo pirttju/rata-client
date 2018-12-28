98cc31dfccfd2519b300a0a567abf31743bed059
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2018-12-28 13:41:42 +0200

    Update ColumnSet

--- consumers/composition.js
+++ consumers/composition.js
@@ -3,8 +3,9 @@ const p = require("../pgconn");
 const u = require ("../utils");
 
 // Reusable set of columns
-const cs_compositions = new p.pgp.helpers.ColumnSet(["train_number", "departure_date"],
-    {schema: "rail", table: "compositions"});
+const cs_timetable_rows = new p.pgp.helpers.ColumnSet(["train_number", "departure_date", "version", "journey_index",
+    "begin_station", "begin_time", "end_station", "end_time", "locomotives", "wagons", "total_length",
+    "maximum_speed", "last_modified"], {table: {schema: "rata", table: "compositions"}});
 
 exports.processMessage = function(json, callback) {
     callback(null);
