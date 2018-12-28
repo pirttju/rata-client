95c20eeefa104068242e77f3f170934972541c92
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2018-12-28 13:38:37 +0200

    Changed message handling

--- consumers/composition.js
+++ consumers/composition.js
@@ -1,34 +1,11 @@
-/*
-Process composition messages.
-*/
+const moment = require("moment");
 const p = require("../pgconn");
 const u = require ("../utils");
-const moment = require("moment");
-const async = require("async");
 
 // Reusable set of columns
 const cs_compositions = new p.pgp.helpers.ColumnSet(["train_number", "departure_date"],
     {schema: "rail", table: "compositions"});
 
-exports.processMessage = function(body, callback) {
-    let data;
-
-    try {
-        data = JSON.parse(body);
-    } catch (e) {
-        return callback("JSON parse failed");
-    }
-
-    async.eachSeries(data, function(item, next) {
-        processComposition(data, function() {
-            next();
-        });
-    }, function(err) {
-        callback();
-    });
+exports.processMessage = function(json, callback) {
+    callback(null);
 }
-
-function processComposition(data, callback) {
-    // TODO
-    callback();
-};
