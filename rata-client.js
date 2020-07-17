70d7433de8d47b9c920ea6a0c23cf6795426b920
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-17 10:55:23 +0300

    Removed compositions and train-locations

--- rata-client.js
+++ rata-client.js
@@ -1,25 +1,17 @@
 const async = require("async");
 const fs = require("fs");
-const moment = require("moment");
-const mqtt = require("mqtt");
 const pollTrains = require("./pollTrains");
 const train = require ("./consumers/train");
-const trainlocation = require ("./consumers/trainlocation");
-const composition = require("./consumers/composition");
-
-let rataMQTTDisconnected = false;
 
 function main(callback) {
     // Check for json file given as command line parameter
     if (process.argv.length > 2) {
         handleFile(process.argv[2], function(err) {
-            // Done
             callback();
         });
     } else {
-        // No parameter given - start polling trains and open MQTT connection
+        // No parameter given - start polling /trains endpoint
         pollTrains.startPolling();
-        listenRataMQTT();
     }
 }
 
@@ -31,20 +23,18 @@ function handleFile(fileName, callback) {
         if (err) {
             callback("Could not read file", fileName);
         } else {
-            let json = null;
+            let data = null;
 
             try {
-                json = JSON.parse(data);
+                data = JSON.parse(data);
             } catch (e) {
                 callback("JSON parse failed");
             }
 
-            async.eachSeries(json, function(item, next) {
-                let fileType = "?";
-                if (item.timeTableRows) fileType = "trains";
-                if (item.journeySections) fileType = "compositions";
-                processMessage(fileType, item, function(err) {
-                    if (err) console.log(moment().toISOString(), err);
+            async.eachSeries(data, function(item, next) {
+                train.processMessage(item, function(err) {
+                    if (err)
+                        console.log(err);
                     next();
                 });
             }, function(err) {
@@ -59,8 +49,9 @@ function handleFile(fileName, callback) {
  */
 pollTrains.on("data", function(data) {
     async.eachSeries(data, function(item, next) {
-        processMessage("trains", item, function(err) {
-            if (err) console.log(moment().toISOString(), err);
+        train.processMessage(item, function(err) {
+            if (err)
+                console.log(err);
             next();
         });
     }, function() {
@@ -69,75 +60,8 @@ pollTrains.on("data", function(data) {
 });
 
 pollTrains.on("error", function(err) {
-    console.log(moment().toISOString(), "error", err)
+    console.log(err)
 });
-
-/**
- * Listens for data over MQTT
- */
-function listenRataMQTT() {
-    const client = mqtt.connect("mqtt://rata-mqtt.digitraffic.fi");
-
-    client.on("connect", function(connack) {
-        if (connack && connack.sessionPresent) {
-            console.log(moment().toISOString(), "MQTT: Connection restored.");
-        } else {
-            console.log(moment().toISOString(), "MQTT: Connected.");
-
-            // Listen compositions and train-locations
-            client.subscribe(["compositions/#", "train-locations/#"], function(err) {
-                if (!err) {
-                    console.log(moment().toISOString(), "MQTT: Subscribed.");
-                    rataMQTTDisconnected = false;
-                } else {
-                    console.error(moment().toISOString(), "MQTT: Subscription failed.");
-                    rataMQTTDisconnected = true;
-                }
-            });
-        }
-    });
-
-    client.on("reconnect", function() {
-        console.log(moment().toISOString(), "MQTT: Reconnecting...");
-    });
-
-    client.on("close",  function() {
-        console.error(moment().toISOString(), "MQTT: Connection closed.");
-        rataMQTTDisconnected = true;
-    });
-
-    client.on("error",  function(err) {
-        console.error(moment().toISOString(), "MQTT: Error", err);
-    });
-
-    client.on("message", function(topic, message) {
-        try {
-            json = JSON.parse(message);
-        } catch (e) {
-            console.log(moment().toISOString(), "JSON parse failed");
-        }
-
-        processMessage(topic, json, function(err) {
-            if (err) console.log(moment().toISOString(), topic, err);
-        });
-    });
-}
-
-function processMessage(topic, json, callback) {
-    if (topic.startsWith("trains")) {
-        train.processMessage(json, function(err) {
-            callback(err);
-        });
-    } else if (topic.startsWith("train-locations")) {
-        trainlocation.processMessage(json, function(err) {
-            callback(err);
-        });
-    } else if (topic.startsWith("compositions")) {
-        composition.processMessage(json, function(err) {
-            callback(err);
-        });
-    }
-}
 
 main(function(err) {
     if (err) {
