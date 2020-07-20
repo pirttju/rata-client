17c663137f2e925e7cb0c70784a547aa4d43b21c
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-20 15:40:52 +0300

    Rewrite some initial code

--- rata-client.js
+++ rata-client.js
@@ -1,73 +1,24 @@
-const async = require("async");
-const fs = require("fs");
-const pollTrains = require("./pollTrains");
-const train = require ("./consumers/train");
+const needle = require("needle");
+const {db} = require("./db");
+const train = require("./consumers/train.js");
 
-function main(callback) {
-    // Check for json file given as command line parameter
-    if (process.argv.length > 2) {
-        handleFile(process.argv[2], function(err) {
-            callback();
-        });
-    } else {
-        // No parameter given - start polling /trains endpoint
-        pollTrains.startPolling();
-    }
+function query(version = null) {
+  const apiurl = "https://rata.digitraffic.fi/api/v1/trains";
+  const params = version !== null ? "?version=" + version.toString() : "";
+  const options = { compressed: true, json: true };
+
+  needle("get", apiurl + params, options)
+  .then(response => {
+    return train.processResult(response.body);
+  })
+  .then(data => {
+    // processed data
+    console.log(data);
+    setTimeout(() => query(data.version), 15000);
+    return;
+  })
+  .catch(err => {
+    // err
+    console.log(err);
+  })
 }
-
-/**
- * Handles a file
- */
-function handleFile(fileName, callback) {
-    fs.readFile(fileName, function(err, data) {
-        if (err) {
-            callback("Could not read file", fileName);
-        } else {
-            let data = null;
-
-            try {
-                data = JSON.parse(data);
-            } catch (e) {
-                callback("JSON parse failed");
-            }
-
-            async.eachSeries(data, function(item, next) {
-                train.processMessage(item, function(err) {
-                    if (err)
-                        console.log(err);
-                    next();
-                });
-            }, function(err) {
-                callback(null);
-            });
-        }
-    });
-}
-
-/**
- * Receives train data
- */
-pollTrains.on("data", function(data) {
-    async.eachSeries(data, function(item, next) {
-        train.processMessage(item, function(err) {
-            if (err)
-                console.log(err);
-            next();
-        });
-    }, function() {
-        return;
-    });
-});
-
-pollTrains.on("error", function(err) {
-    console.log(err)
-});
-
-main(function(err) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    } else {
-        process.exit(0);
-    }
-});
