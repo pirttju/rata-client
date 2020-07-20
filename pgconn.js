f7124f70e2418868ac4c6e13660b17e0eff46f2b
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-20 15:42:31 +0300

    Delete pgconn.js

--- pgconn.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = require("../db-config.json");
-const pgPromise = require("pg-promise");
-
-const initOptions = {};
-const pgp = pgPromise(initOptions);
-const db = pgp(dbConfig);
-
-module.exports = { pgp, db };
