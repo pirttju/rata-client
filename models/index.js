81f6f6bf1200d037301f9a7cf3924eb664e08ac1
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-24 14:36:20 +0300

    delete old versions on upsert

--- models/index.js
+++ models/index.js
@@ -3,11 +3,15 @@ const {db} = require("../db");
 function upsertTrains(data) {
   return db.tx("upsert-trains", t => {
     const queries = [];
-    if (data.trains && data.trains.length > 0)
-      queries.push(t.trains.upsert(data.trains));
     if (data.timetablerows && data.timetablerows.length > 0)
       queries.push(t.timetablerows.upsert(data.timetablerows));
-    return t.batch(queries);
+    if (data.trains && data.trains.length > 0)
+      queries.push(t.trains.upsert(data.trains));
+      for (const train of data.trains) {
+        queries.push(t.timetablerows.deleteOldVersions(train.departure_date, train.train_number, train.version));
+      }
+    return t.batch(queries)
+    });
   });
 }
 
@@ -16,6 +20,9 @@ function upsertCompositions(data) {
     const queries = [];
     if (data.compositions && data.compositions.length > 0)
       queries.push(t.compositions.upsert(data.compositions));
+      for (const train of data.trains) {
+        queries.push(t.compositions.deleteOldVersions(train.departure_date, train.train_number, train.version));
+      }
     return t.batch(queries);
   });
 }
