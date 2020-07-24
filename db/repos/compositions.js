ac510a37cbc0065bea994956e91796f8ed2aabac
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-24 13:40:32 +0300

    Create compositions.js

---
+++ db/repos/compositions.js
@@ -0,0 +1,59 @@
+const cs = {};
+
+function createColumnsets(pgp) {
+  if (!cs.insert) {
+    cs.insert = new pgp.helpers.ColumnSet([
+      "departure_date",
+      "train_number",
+      "journey_section",
+      "version",
+      "begin_station",
+      "begin_time",
+      "end_station",
+      "end_time",
+      "locomotives",
+      "wagons",
+      "total_length",
+      "maximum_speed",
+      {name: "last_modified", mod: "^", def: "current_timestamp"}
+    ], {table: {table: "trains", schema: "public"}});
+  }
+}
+
+class CompositionsRepository {
+  constructor(db, pgp) {
+    this.db = db;
+    this.pgp = pgp;
+    createColumnsets(pgp);
+  }
+
+  async insert(data) {
+    const query = this.pgp.helpers.insert(data, cs.insert);
+    return this.db.none(query);
+  }
+
+  async maxversion() {
+    return this.db.one("select max(version) from compositions");
+  }
+
+  async upsert(data) {
+    const query = this.pgp.helpers.insert(data, cs.insert) +
+      " on conflict (departure_date, train_number, journey_section) do update set " +
+      cs.insert.assignColumns({from: 'excluded', skip: ["departure_date", "train_number", "journey_section"]});
+    return this.db.none(query);
+  }
+
+  async delete(date, number) {
+    return this.db.result("delete from compositions where departure_date = $1 and train_number = $2", [date, +number], r => r.rowCount);
+  }
+
+  async deleteOldVersions(date, number, version) {
+    return this.db.result("delete from compositions where departure_date = $1 and train_number = $2 and version < $3", [date, +number, +version], r => r.rowCount);
+  }
+
+  async deleteByDate(date) {
+    return this.db.result("delete from compositions where departure_date = $1", [date], r => r.rowCount);
+  }
+}
+
+module.exports = CompositionsRepository;
