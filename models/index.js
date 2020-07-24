0b5a07fc5fb2a28886a483a625f7d545ae7f9643
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-24 14:41:46 +0300

    fix if blocks

--- models/index.js
+++ models/index.js
@@ -3,13 +3,18 @@ const {db} = require("../db");
 function upsertTrains(data) {
   return db.tx("upsert-trains", t => {
     const queries = [];
-    if (data.timetablerows && data.timetablerows.length > 0)
+
+    if (data.timetablerows && data.timetablerows.length > 0) {
       queries.push(t.timetablerows.upsert(data.timetablerows));
-    if (data.trains && data.trains.length > 0)
+    }
+
+    if (data.trains && data.trains.length > 0) {
       queries.push(t.trains.upsert(data.trains));
       for (const train of data.trains) {
         queries.push(t.timetablerows.deleteOldVersions(train.departure_date, train.train_number, train.version));
       }
+    }
+
     return t.batch(queries);
   });
 }
@@ -17,11 +22,14 @@ function upsertTrains(data) {
 function upsertCompositions(data) {
   return db.tx("upsert-compositions", t => {
     const queries = [];
-    if (data.compositions && data.compositions.length > 0)
+
+    if (data.compositions && data.compositions.length > 0) {
       queries.push(t.compositions.upsert(data.compositions));
       for (const train of data.trains) {
         queries.push(t.compositions.deleteOldVersions(train.departure_date, train.train_number, train.version));
       }
+    }
+
     return t.batch(queries);
   });
 }
