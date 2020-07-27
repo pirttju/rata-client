73340af2a750c4e86dad6e4cdee5f66ffbc0d904
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-27 15:48:18 +0300

    hypertable intervals and approx sizes on disk

--- init.sql
+++ init.sql
@@ -19,6 +19,8 @@ CREATE TABLE trains (
 );
 
 SELECT create_hypertable('trains', 'departure_date');
+-- volume 2000 rows per day: 370 kB on disk
+-- 1 year = 132 MB
 SELECT set_chunk_time_interval('trains', interval '1 year');
 
 
@@ -54,7 +56,9 @@ CREATE TABLE timetablerows (
 CREATE INDEX ON timetablerows (station);
 
 SELECT create_hypertable('timetablerows', 'departure_date');
-SELECT set_chunk_time_interval('timetablerows', interval '2 months');
+-- volume 40000 rows per day: 9.7 MB on disk
+-- 4 months = 885 MB
+SELECT set_chunk_time_interval('timetablerows', interval '4 months');
 
 
 -- composition data for trains
@@ -76,4 +80,6 @@ CREATE TABLE compositions (
 );
 
 SELECT create_hypertable('compositions', 'departure_date');
+-- volume 1000 rows per day: 1 MB on disk
+-- 1 year = 365 MB
 SELECT set_chunk_time_interval('compositions', interval '1 year');
