ee76438cea22910a7240878775786c798b63f68b
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2020-07-29 13:33:07 +0300

    file import function

--- rata-client.js
+++ rata-client.js
@@ -3,12 +3,13 @@ const argv = require("minimist")(process.argv.slice(2));
 const train = require("./consumers/train.js");
 const composition = require("./consumers/composition.js");
 
-// start polling
+// starts polling
 function poll() {
   train.start();
   composition.start();
 }
 
+// checks if file exists
 function fileExists(file) {
   try {
     if (fs.existsSync(path)) {
@@ -20,23 +21,31 @@ function fileExists(file) {
   return false;
 }
 
-// import trains from a file
-function importTrains(file) {
-  // todo
+// parses json from a file and runs import
+async function import(file, consumer) {
+  const fileContents = fs.readFileSync(file, 'utf8');
+
+  try {
+    const data = JSON.parse(fileContents);
+  } catch(err) {
+    console.error(err);
+    return false;
+  }
+
+  if (data && data.length > 0) {
+    const result = await consumer.processResult(data);
+    console.log(result);
+  } else {
+    console.error("No data");
+    return false;
+  }
 }
 
-// import compositions from a file
-function importCompositions(file) {
-  // todo
-}
-
-// parse command line arguments
-if (argv.c) {
-  if (fileExists(argv.c)
-    importCompositions(argv.c);
-} else if (argv.t) {
-  if (fileExists(argv.t)
-    importTrains(argv.t);
+// checks for command line arguments
+if (argv.c && fileExists(argv.c)) {
+  import(argv.c, composition);
+} else if (argv.t && fileExists(argv.t)) {
+  import(argv.t, train);
 } else if (argv.poll) {
   poll();
 } else {
