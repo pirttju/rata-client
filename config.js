7974586d879fb5df342b15c5ad7784791f05e6ac
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2021-01-22 11:24:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,11 @@ const config = {};
 
 config.DIGITRAFFIC_USER = "User/App"; // change to your liking
 config.USER_AGENT = "RataClient/2.0";
-
+config.DB = {
+  "host": "localhost",
+  "port": 5432,
+  "database": "db",
+  "user": "usr",
+  "password": "pass"
+}
 module.exports = config;
