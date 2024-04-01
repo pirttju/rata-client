f6560dabe9d0b41a41caf3dbbe75565755e9460f
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2024-04-01 14:54:14 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,68 +1,54 @@
--- train data
-CREATE TABLE trains (
-    departure_date      date NOT NULL,
-    train_number        int NOT NULL,
-    version             bigint NOT NULL,
-    train_type          text NOT NULL,
-    commuter_line_id    text,
-    operator_code       text,
-    running_currently   boolean NOT NULL DEFAULT FALSE,
-    cancelled           boolean NOT NULL DEFAULT FALSE,
-    adhoc_timetable     boolean NOT NULL DEFAULT FALSE,
-    acceptance_date     timestamptz NOT NULL,
-    begin_station       text NOT NULL,
-    begin_time          timestamptz NOT NULL,
-    end_station         text NOT NULL,
-    end_time            timestamptz NOT NULL,
-    last_modified       timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE SCHEMA IF NOT EXISTS digitraffic;
+
+-- Trains
+CREATE TABLE IF NOT EXISTS digitraffic.train (
+    departure_date              date NOT NULL,
+    train_number                int NOT NULL,
+    operator_uic_code           smallint NOT NULL,
+    train_type                  text NOT NULL,
+    commuter_line_id            text,
+    running_currently           boolean NOT NULL,
+    cancelled                   boolean NOT NULL,
+    version                     bigint NOT NULL,
+    timetable_type              text NOT NULL,
+    timetable_acceptance_date   timestamp with time zone NOT NULL,
+    deleted                     boolean,
     PRIMARY KEY (departure_date, train_number)
 );
 
--- timetable data for trains
-CREATE TABLE timetablerows (
-    departure_date      date NOT NULL,
-    train_number        int NOT NULL,
-    row_index           smallint NOT NULL,
-    version             bigint NOT NULL,
-    station             text NOT NULL,
-    train_stopping      boolean NOT NULL DEFAULT FALSE,
-    commercial_stop     boolean NOT NULL DEFAULT FALSE,
-    commercial_track    text,
-    arr_scheduled       timestamptz,
-    arr_actual          timestamptz,
-    arr_diff            smallint,
-    arr_is_estimate     boolean,
-    arr_unknown_delay   boolean,
-    arr_cancelled       boolean,
-    arr_cause           text,
-    dep_scheduled       timestamptz,
-    dep_actual          timestamptz,
-    dep_diff            smallint,
-    dep_is_estimate     boolean,
-    dep_unknown_delay   boolean,
-    dep_cancelled       boolean,
-    dep_cause           text,
-    train_ready         timestamptz,
-    train_ready_src     text,
-    PRIMARY KEY (departure_date, train_number, row_index)
+-- Time Table Rows
+CREATE TABLE IF NOT EXISTS digitraffic.time_table_row (
+    departure_date              date NOT NULL,
+    train_number                integer NOT NULL,
+    row_number                  smallint NOT NULL,
+    station_short_code          text NOT NULL,
+    type                        text NOT NULL,
+    train_stopping              boolean NOT NULL,
+    commercial_stop             boolean,
+    commercial_track            text,
+    cancelled                   boolean NOT NULL,
+    scheduled_time              timestamp with time zone NOT NULL,
+    live_estimate_time          timestamp with time zone,
+    unknown_delay               boolean,
+    actual_time                 timestamp with time zone,
+    difference_in_minutes       smallint,
+    causes                      text,
+    train_ready                 boolean,
+    PRIMARY KEY (departure_date, train_number, row_number),
+    FOREIGN KEY (departure_date, train_number) REFERENCES digitraffic.train (departure_date, train_number) ON DELETE CASCADE
 );
 
-CREATE INDEX ON timetablerows (station);
-
--- composition data for trains
-CREATE TABLE compositions (
-    departure_date      date NOT NULL,
-    train_number        integer NOT NULL,
-    journey_section     smallint NOT NULL,
-    version             bigint NOT NULL,
-    begin_station       text NOT NULL,
-    begin_time          timestamptz NOT NULL,
-    end_station         text NOT NULL,
-    end_time            timestamptz NOT NULL,
-    locomotives         jsonb,
-    wagons              jsonb,
-    total_length        smallint,
-    maximum_speed       smallint,
-    last_modified       timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (departure_date, train_number, journey_section)
+-- Compositions
+CREATE TABLE IF NOT EXISTS digitraffic.composition (
+    departure_date              date NOT NULL,
+    train_number                integer NOT NULL,
+    composition_number          smallint NOT NULL,
+    begin_station_short_code    text NOT NULL,
+    end_station_short_code      text NOT NULL,
+    locomotives                 jsonb NOT NULL,
+    wagons                      jsonb,
+    total_length                smallint NOT NULL,
+    maximum_speed               smallint NOT NULL,
+    PRIMARY KEY (departure_date, train_number, composition_number),
+    FOREIGN KEY (departure_date, train_number) REFERENCES digitraffic.train (departure_date, train_number) ON DELETE CASCADE
 );
