108f9d1f276fbc2976672ca13ac08f7111984619
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2024-04-01 15:14:28 +0300

    Update compositions.js

--- db/repos/compositions.js
+++ db/repos/compositions.js
@@ -5,18 +5,16 @@ function createColumnsets(pgp) {
     cs.insert = new pgp.helpers.ColumnSet([
       "departure_date",
       "train_number",
-      "journey_section",
+      "composition_number",
       "version",
-      "begin_station",
-      "begin_time",
-      "end_station",
-      "end_time",
+      "begin_station_short_code",
+      "end_station_short_code",
       {name: "locomotives", mod: ":json"},
       {name: "wagons", mod: ":json"},
       "total_length",
       "maximum_speed",
       {name: "last_modified", mod: "^", def: "current_timestamp"}
-    ], {table: {table: "compositions", schema: "public"}});
+    ], {table: {table: "composition", schema: "digitraffic"}});
   }
 }
 
@@ -33,26 +31,26 @@ class CompositionsRepository {
   }
 
   async getMaxVersion() {
-    return this.db.one("select max(version) from compositions");
+    return this.db.one("select max(version) from digitraffic.composition");
   }
 
   async upsert(data) {
     const query = this.pgp.helpers.insert(data, cs.insert) +
-      " on conflict (departure_date, train_number, journey_section) do update set " +
-      cs.insert.assignColumns({from: 'excluded', skip: ["departure_date", "train_number", "journey_section"]});
+      " on conflict (departure_date, train_number, composition_number) do update set " +
+      cs.insert.assignColumns({from: 'excluded', skip: ["departure_date", "train_number", "composition_number"]});
     return this.db.none(query);
   }
 
   async delete(date, number) {
-    return this.db.result("delete from compositions where departure_date = $1 and train_number = $2", [date, +number], r => r.rowCount);
+    return this.db.result("delete from digitraffic.composition where departure_date = $1 and train_number = $2", [date, +number], r => r.rowCount);
   }
 
   async deleteOldVersions(date, number, version) {
-    return this.db.result("delete from compositions where departure_date = $1 and train_number = $2 and version < $3", [date, +number, +version], r => r.rowCount);
+    return this.db.result("delete from digitraffic.composition where departure_date = $1 and train_number = $2 and version < $3", [date, +number, +version], r => r.rowCount);
   }
 
   async deleteByDate(date) {
-    return this.db.result("delete from compositions where departure_date = $1", [date], r => r.rowCount);
+    return this.db.result("delete from digitraffic.composition where departure_date = $1", [date], r => r.rowCount);
   }
 }
 
